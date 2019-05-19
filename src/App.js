26915badc3440594a79eed640ea57c9e7328357c
Sam <juntilla@hawaii.edu>
2019-05-18 19:56:11 -1000

    adds gallery title bars

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/footer";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import "./App.css";
@@ -19,18 +20,23 @@ function App() {
             {/* maybe use .map on database items here? */}
             <GridListTile>
               <img src="https://i.imgur.com/CC4EFLz.jpg" />
+              <GridListTileBar title="FUCK" />
             </GridListTile>
             <GridListTile>
               <img src="https://i.imgur.com/4kSDdjn.jpg" />
+              <GridListTileBar title="AAAAAA" />
             </GridListTile>
             <GridListTile>
               <img src="https://i.ytimg.com/vi/3z2EzQvpbok/maxresdefault.jpg" />
+              <GridListTileBar title="FUUUUCK" />
             </GridListTile>
             <GridListTile>
               <img src="https://cdn1.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg" />
+              <GridListTileBar title="OH GOOOOD" />
             </GridListTile>
             <GridListTile>
               <img src="https://cdn.cnn.com/cnnnext/dam/assets/150324154010-04-internet-cats-restricted-super-169.jpg" />
+              <GridListTileBar title="I'M ON FIIIRE" />
             </GridListTile>
           </GridList>
         </div>
