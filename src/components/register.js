0ad53e81b9d0f6fd012d199e1a5eb853918a91f9
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-Air.home>
2019-05-25 09:07:56 -1000

    still working on form for register

--- src/components/register.js
+++ src/components/register.js
@@ -10,12 +10,72 @@ class Register extends Component {
     super(props);
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      username: "",
+      first_name: "",
+      last_name: ""
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    fetch("http://localhost:8080/user", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        first_name: this.state.first_name,
+        last_name: this.state.last_name,
+        email: this.state.email,
+        password: this.state.password
+      })
+    })
+      .then(() => {
+        console.log("User added to database!!");
+      })
+      .catch(err => {
+        console.log("error", err);
+      });
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    console.log(this.state);
+  };
+
   render() {
-    return <div>The register page</div>;
+    const { email, password } = this.state;
+    return (
+      <form>
+        <div>
+          <label>
+            Email:
+            <input onChange={this.handleChange} type="text" name="email" />
+          </label>
+          <label>
+            Password:
+            <input onChange={this.handleChange} type="text" name="password" />
+          </label>
+          <label>
+            Username:
+            <input type="text" name="username" />
+          </label>
+          <label>
+            First Name:
+            <input type="text" name="first_name" />
+          </label>
+          <label>
+            Last Name:
+            <input type="text" name="last_name" />
+          </label>
+        </div>
+      </form>
+    );
   }
 }
 
