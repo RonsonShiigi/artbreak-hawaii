d7fde7f55168a92f09c94b8a028695bc021595c3
Sam <juntilla@hawaii.edu>
2019-05-25 07:28:35 -1000

    starts playing with register form, cleans up code

--- src/components/register.js
+++ src/components/register.js
@@ -1,22 +1,58 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import GridListTileBar from "@material-ui/core/GridListTileBar";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
 
-class Register extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: ""
-    };
-  }
+function Register() {
+  const [values, setValues] = React.useState({
+    email: "",
+    password: ""
+  });
 
-  render() {
-    return <div>The register page</div>;
-  }
+  const handleChange = name => e => {
+    setValues({ ...values, [name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <form>
+        <TextField
+          id="email"
+          label="email"
+          value={values.email}
+          onChange={handleChange("email")}
+          margin="normal"
+        />
+      </form>
+    </div>
+  );
 }
+// class Register extends Component {
+// constructor(props) {
+//   super(props);
+//   this.state = {
+//     email: "",
+//     password: ""
+//   };
+// }
+
+//   render() {
+//     return (
+//       <div>
+//         <form>
+//           <TextField
+//             id="email"
+//             label="email"
+//             value={values.email}
+//             onChange={handleChange("email")}
+//             margin="normal"
+//           />
+//         </form>
+//       </div>
+//     );
+//   }
+// }
 
 export default Register;
