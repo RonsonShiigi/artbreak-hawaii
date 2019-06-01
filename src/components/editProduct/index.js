59d9bce80eca8df1a7bde78ae0bfc7c7e6564418
Ronson Shiigi <ronsonshiigi@Ronsons-MBP.home>
2019-06-01 12:45:00 -1000

    edit via browser works, must make dynamic to product id

--- src/components/editProduct/index.js
+++ src/components/editProduct/index.js
@@ -16,7 +16,7 @@ class FileEdit extends Component {
       user_id: "",
       price: "",
       photos: null,
-      product_id: 6
+      product_id: 12
     };
   }
 
@@ -26,40 +26,113 @@ class FileEdit extends Component {
     this.setState({ [name]: e.target.value });
   };
 
-  submitFile = event => {
-    event.preventDefault();
-    console.log("this is state", this.state);
+  // submitFile = event => {
+  //   event.preventDefault();
+  //   console.log("this is state", this.state);
+  //   const url = "https://s3-us-west-2.amazonaws.com/artbreakjeh/";
+  //   // const image_url = url + res.req.file.key;
+
+  //   //posting to s3 axios call
+  //   const formData = new FormData();
+  //   console.log("formData", formData);
+
+  //   formData.append("file", this.state.file[0]);
+  //   axios
+  //     .post(
+  //       "http://localhost:8080/products/" + this.state.product_id,
+  //       formData,
+  //       {
+  //         headers: {
+  //           "Content-Type": "multipart/form-data"
+  //         }
+  //       }
+  //     )
+  //     .then(response => {
+  //       console.log("response", response.data.key);
+  //     });
+
+  //   // posting to postgresql axios request
+  //   // axios
+  //   //   .post("http://localhost:8080/products", this.state)
+  //   //   .then(res => {
+  //   //     console.log("response", res.data);
+  //   //   })
+  //   //   .catch(err => {
+  //   //     console.log("error in creating a new product", err);
+  //   //   });
+  // };
+  editFile = e => {
+    e.preventDefault();
+    console.log("you are trying to delete");
     const url = "https://s3-us-west-2.amazonaws.com/artbreakjeh/";
-    // const image_url = url + res.req.file.key;
-
-    //posting to s3 axios call
     const formData = new FormData();
-    console.log("formData", formData);
-
-    formData.append("file", this.state.file[0]);
     axios
-      .post(
-        "http://localhost:8080/products/" + this.state.product_id,
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data"
+      .get("http://localhost:8080/products")
+      .then(res => {
+        let products = res.data;
+        products.filter(product => {
+          if (product.id === this.state.product_id) {
+            this.state.product = product;
           }
-        }
-      )
-      .then(response => {
-        console.log("response", response.data.key);
-      });
+        });
+      })
+      .then(data => {
+        this.state.image_url = this.state.product.image_url;
+        console.log("imageURL", this.state.image_url);
+      })
+      .then(data => {
+        console.log("this is state", this.state);
+      })
+      .then(data => {
+        axios
+          .delete("http://localhost:8080/products/" + this.state.product_id, {
+            data: { body: this.state }
+          })
+          .then(data => {
+            console.log("you are deleting from s3 and psql");
+          })
+          // .then(res => {
+          //   res.redirect("/");
+          // })
+          .catch(err => {
+            console.log(err);
+          });
+      })
+      .then(data => {
+        console.log("this is state", this.state);
+        formData.append("file", this.state.file[0]);
+        axios
+          .post("http://localhost:8080/newProduct/fiyah", formData, {
+            headers: {
+              "Content-Type": "multipart/form-data"
+            }
+          })
+          .then(response => {
+            console.log("response", response.data.key);
+            let key = response.data.key;
+            this.state.image_url = url + key;
+          })
+          .then(data => {
+            console.log("updated state", this.state);
+          })
+          .then(data => {
+            axios
+              .post("http://localhost:8080/products", this.state)
+              .then(res => {
+                console.log("response", res.data);
+              })
+              .catch(err => {
+                console.log("error in creating a new product", err);
+              });
+          })
+          .catch(error => {
+            // handle your error
+          });
+      })
 
-    // posting to postgresql axios request
-    // axios
-    //   .post("http://localhost:8080/products", this.state)
-    //   .then(res => {
-    //     console.log("response", res.data);
-    //   })
-    //   .catch(err => {
-    //     console.log("error in creating a new product", err);
-    //   });
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   handleFileEdit = event => {
@@ -69,7 +142,7 @@ class FileEdit extends Component {
   render() {
     return (
       <div class="upload-form">
-        <form onSubmit={this.submitFile}>
+        <form onSubmit={this.editFile}>
           <input
             label="upload file"
             type="file"
