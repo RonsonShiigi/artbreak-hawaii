93a179fe7f3210a0b9afc09a6a67585b7e0bff50
Sam <juntilla@hawaii.edu>
2019-06-08 07:14:51 -1000

    consolidates logged in/out state of header

--- server/database/migrations/20190518122435_users.js
+++ server/database/migrations/20190518122435_users.js
@@ -13,6 +13,7 @@ exports.up = function(knex, Promise) {
     table.string("first_name");
     table.string("last_name");
     table.string("stripe_id");
+    table.string("profileblurb");
     table.timestamp("created_at").defaultTo(knex.raw("now()"));
     table.timestamp("updated_at");
   });
