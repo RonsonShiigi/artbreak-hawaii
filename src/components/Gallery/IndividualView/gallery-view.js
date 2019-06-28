9eabeb1fc73cc969e146d25033b4864597de8667
Samantha Juntilla <44386319+sjuntilla@users.noreply.github.com>
2019-06-27 19:01:32 -1000

    Update gallery-view.js

--- src/components/Gallery/IndividualView/gallery-view.js
+++ src/components/Gallery/IndividualView/gallery-view.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Comments from "../../Comments/comments";
-import Likes from "../../Likes/Likes";
+import Likes from "../../likes/likes";
 import { Link } from "react-router-dom";
 
 import "./gallery-view.css";
